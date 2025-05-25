52ecfadfc5421f44ce29b286afee4cea00b64062
Freda Manu <freda.manu@fyndreality.com>
2025-05-25 12:51:01 +0200

    adding all

---
+++ src/components/modules/FyndHeader.jsx
@@ -0,0 +1,14 @@
+export function Component() {
+  return (
+    <header className="fynd-header">
+   <h1>Hello</h1>
+    </header>
+  );
+}
+
+export const fields = [];
+
+export const meta = {
+  label: 'Fynd Header',
+  
+};
